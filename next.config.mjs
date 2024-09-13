7491ec9cc8a649fdfc80d1741922493d9068e31a
Zachary Mallicoat <zacharym123@gmail.com>
2024-09-13 01:29:56 -0600

    basepath change

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/deadlockdraft-web",
+  basePath: "http://deadlockdraft.com/",
 
   /**
    * Disable server-based image optimization. Next.js does not support
